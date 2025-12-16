252112500484b0bbcb0539a0d3e8725c8c510d8a
hi <hi@DESKTOP-F4R4LPM>
2025-12-16 08:03:54 -0800

    commit 1

---
+++ Java_Code/src/com/tnsif/basicss/ForLoopExample.java
@@ -0,0 +1,16 @@
+package com.tnsif.basicss;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<=1000; i--)
+		{
+			System.out.println("Value of i:");
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
