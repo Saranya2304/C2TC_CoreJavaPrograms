24a07ae9cb410095f8b24c54464cdbd220f47301
hi <hi@DESKTOP-F4R4LPM>
2025-12-17 05:10:46 -0800

    commit 2

---
+++ Java_Code/src/com/tnsif/oops/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.oops.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// Object Creation
+		OopsConceptDemo obj=new OopsConceptDemo(); //reference variable
+		obj.setSerialNum(101);
+		obj.setName("Saranya");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
