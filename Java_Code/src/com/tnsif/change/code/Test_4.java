5f75326151e62d49feb74fda4c2c6e24f978ff78
hi <hi@DESKTOP-F4R4LPM>
2025-12-22 07:10:47 -0800

    commit 8

--- Java_Code/src/com/tnsif/change/code/Test_4.java
+++ Java_Code/src/com/tnsif/change/code/Test_4.java
@@ -1,5 +1,20 @@
 package com.tnsif.change.code;
 
-public class Test_4 {
-
+class A2 {
+    int x = 10;
 }
+
+class B2 extends A2 {
+    int x = 20;
+    
+    @Override
+    public String toString() {
+        //System.out.println(x);  
+        return "x="+x;
+    }
+    public static void main(String[] args) {
+		B2 b = new B2();
+		System.out.println(b.toString());
+	}
+}
+
